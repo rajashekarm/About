7334a90542621132b20f1010f37c2c69720a76ec
rajashekarm <94944999+rajashekarm@users.noreply.github.com>
2023-06-18 23:42:30 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -26,7 +26,7 @@ local_css("style.css")
 
 # ---- HEADER SECTION ----
 with st.container():
-    st.subheader("Hey!, I am Raj🙌 :wave:")
+    st.subheader("Hey!, I am Raj:wave: 🙌")
     st.title("Aspiring Python devoloper")
     st.write(
 "As a third-year electrical and electronics engineering student, I am driven to explore the world of software development. While my electrical core provides me with a strong foundation, I am also learning data structures and algorithms (DSA) using Python to further enhance my skills in the software field. "
@@ -34,7 +34,7 @@ with st.container():
 " With a hardworking and productive attitude, I am constantly seeking opportunities to learn and grow. As the chairperson of a student club, I have gained invaluable experience in leadership, teamwork, and communication - skills that I believe will be essential in any future role. "
 
     )
-    st.write("[Know more about me>](https://www.linkedin.com/in/rajashekar-m-1739501b1)")
+    st.write("[contact me>](9353256651)")
 
 # ---- WHAT I DO ----
 with st.container():
