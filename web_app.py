fd9b2b63c69351cee6244770742460c85c3b970b
rajashekarm <94944999+rajashekarm@users.noreply.github.com>
2023-09-09 12:50:45 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -95,8 +95,7 @@ with st.container():
     with text_column:
         st.write(
             """
-        1. [Cricket Outcome Predictor: Unveiling Victory with Machine Learning](https://victory-predictor-raj.streamlit.app):
-	
+        1. [Cricket Outcome Predictor: Unveiling Victory with Machine Learning:](https://victory-predictor-raj.streamlit.app)
 		* Developed a machine learning-based Victory Predictor application for IPL matches.
         	* Utilized Python, Streamlit, and Scikit-Learn for project development.
         	* Predicted winning probabilities for IPL teams using logistic regression.
